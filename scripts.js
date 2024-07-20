e10b7c22cfeffb76101dc9d4f42754318e785f49
yashpanchal01 <yashpanchal251198@gmail.com>
2024-07-20 23:03:36 +0530

    chatGPT done

--- scripts.js
+++ scripts.js
@@ -1,3 +1,30 @@
 document.addEventListener('DOMContentLoaded', function() {
-    console.log('Welcome to My GitHub Pages Site!');
+    // JavaScript to animate the typewriter effect
+    const typewriterElement = document.querySelector('.typewriter');
+    const textArray = ['Video Editor', 'Graphic Designer', 'Content Creator'];
+    let arrayIndex = 0;
+    let charIndex = 0;
+
+    function type() {
+        if (charIndex < textArray[arrayIndex].length) {
+            typewriterElement.textContent += textArray[arrayIndex].charAt(charIndex);
+            charIndex++;
+            setTimeout(type, 150);
+        } else {
+            setTimeout(erase, 1500);
+        }
+    }
+
+    function erase() {
+        if (charIndex > 0) {
+            typewriterElement.textContent = textArray[arrayIndex].substring(0, charIndex - 1);
+            charIndex--;
+            setTimeout(erase, 100);
+        } else {
+            arrayIndex = (arrayIndex + 1) % textArray.length;
+            setTimeout(type, 150);
+        }
+    }
+
+    setTimeout(type, 1500);
 });
